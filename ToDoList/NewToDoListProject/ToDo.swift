d3082ee8442e610e9dc370e47187969a99a52c1f
AZcodes <51551780+AZcodes@users.noreply.github.com>
2020-08-11 22:21:24 -0400

    iteration 0

---
+++ ToDoList/NewToDoListProject/ToDo.swift
@@ -0,0 +1,14 @@
+//
+//  ToDo.swift
+//  NewToDoListProject
+//
+//  Created by Apple on 8/11/20.
+//  Copyright © 2020 Ayesha Zaker. All rights reserved.
+//
+
+//import Foundation
+import UIKit
+class ToDoClass {
+    var description = ""
+    var important = false
+}
